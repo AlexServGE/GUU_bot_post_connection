563faf29591b599287ad3bcfced1fa5d989d3b4c
AlexServGE <alexander.sergeevru@yandex.ru>
2024-08-28 19:43:33 +0300

    Corrected user_info_captured 2

--- BotSql/User_info_captured/User_info_captured.py
+++ BotSql/User_info_captured/User_info_captured.py
@@ -1,9 +1,10 @@
 class User:
 
+    #DELETE user_GRADDATE=None,user_INSTITUTE=None,user_EMPLOYER=None из аргументов
     def __init__(self, user_date_of_first_registration=None, user_telegram_id=None, telegram_nickname=None,
                  user_PERSONAL_INFO_ACCEPTANCE=None, user_GENDER=None, user_SURNAME=None,
                  user_NAME=None, user_PATRONYMIC=None, user_EMAIL=None, user_PHONE=None, user_BIRTHDATE=None,
-                 user_GRADDATE=None, user_INSTITUTE=None,
+                 user_GRADDATE=None,
                  user_EMPLOYER=None, user_POSITION=None):
         self.user_date_of_first_registration = user_date_of_first_registration  # 2023-11-05
         self.user_telegram_id = user_telegram_id
